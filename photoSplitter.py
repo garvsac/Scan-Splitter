37c33c8df398aef758dd96e8b7da3b529959bef7
garvsac <garv.sachdeva18@gmail.com>
2020-10-04 16:26:17 +0530

    Final changes

--- photoSplitter.py
+++ photoSplitter.py
@@ -1,8 +1,17 @@
 from scanner import splitPhotos
 import glob
 
-photos = glob.glob(".\\resources\\*")
+photos = glob.glob(".\\resources\\*.jpg")
+
+#debug
+# 0-show nothing
+# 1-show everything
+# 2-show output
+
+#write
+# 0-do nothing
+# 1-save output
 
 #split photo, if okay save if not flag that photo and move on
 for photo in photos:
-    splitPhotos(photo)
+    splitPhotos(photo, debug = 0, write=1)
--- scanner.py
+++ scanner.py
@@ -4,24 +4,28 @@ import numpy as np
 from transform import four_point_transform
 from colorCorrect import simplest_cb, adjust_gamma
 
-debug = 0
+
 output = 1
 
-def splitPhotos(path):
+def splitPhotos(path, debug=0, write=0):
     image = cv2.imread(path)
     #Crop left noise
-    image = image[0:, 15:]
+    image = image[0:, 25:]
     #Add border
     image= cv2.copyMakeBorder(image,50,50,50,50,cv2.BORDER_CONSTANT,value=[255,255,255])
     #cv2.imshow('bordered image',image)
     #cv2.waitKey(0)
+    orig = image.copy()
+
+    new_height = 1200
+    ratio = image.shape[0] / new_height
+    image = imutils.resize(image, height = new_height)
+
     if debug == 1:
-        image = imutils.resize(image, height = 1000)
-        cv2.imshow('image',image)
+        cv2.imshow('image', image)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
-    orig = image.copy()
 
     #Create a mask
     lower_black = np.array([240,240,240], dtype = "uint16")
@@ -33,6 +37,9 @@ def splitPhotos(path):
         cv2.destroyAllWindows()
 
     #Erode and Dilate to remove noise
+    #mask = cv2.erode(black_mask, None, iterations=1)
+    #mask = cv2.dilate(mask, None, iterations=1)
+
     mask = cv2.dilate(black_mask, None, iterations=6)
     if debug == 1:
         cv2.imshow('mask',mask)
@@ -54,32 +61,43 @@ def splitPhotos(path):
     #Find contours and keep the largets ones (except the largest one and remove the unwanted ones)
     cnts = cv2.findContours(blurred.copy(), cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
     cnts = imutils.grab_contours(cnts)
-    cnts = sorted( (c for c in cnts if cv2.arcLength(c, True) > 500) , key = cv2.contourArea, reverse = True)[1:]
+    cnts = sorted( (c for c in cnts if cv2.arcLength(c, True) > 750) , key = cv2.contourArea, reverse = True)[1:]
 
     #Loop over the contours
-    print( "Number of Photos:" + str(len(cnts)) )
+    name = path.split('\\')[-1].split('.')[0]
+    count = 0
     for c in cnts:
         # approximate the contour
         peri = cv2.arcLength(c, True)
-        print("Perimeter:" + str(peri) )
+        #print("Perimeter:" + str(peri) )
         approx = cv2.approxPolyDP(c, 0.06 * peri, True)
         cv2.drawContours(image, [approx], -1, (0, 255, 0), 2)
         # if our approximated contour has four points, then we
         # can assume that we have a photo
         if len(approx) == 4 :
-            print("Photo: True")
+            #print("Photo: True")
             screenCnt = approx
-            photo = four_point_transform(orig, screenCnt.reshape(4, 2))
+            photo = four_point_transform(orig, screenCnt.reshape(4, 2)*ratio)
             out = simplest_cb(photo, 1)
             if output == 1:
                 temp1 = imutils.resize(photo, height = 600)
-                cv2.imshow("Cropped", temp1)
+                #cv2.imshow("Cropped", temp1)
                 temp2 = imutils.resize(out, height = 600)
-                cv2.imshow("Corrected", temp2)
-                cv2.waitKey(0)
-            #print(cv2.imwrite(".\\resources\\a.jpg", photo))
+                #cv2.imshow("Corrected", temp2)
+                #cv2.waitKey(0)
+            count += 1
+            if(write == 1):
+                cv2.imwrite(".\\resources\\output\\" + name + "-" + str(count) + ".jpg", photo)
 
-    if debug == 1:
+    print( name + ": " + str(count))
+
+    if len(cnts) - count != 0:
+        print( "*********************ERROR: " + name )
+        print( "Potential Photos: " + str(len(cnts)) )
+        print( "Actual Photos: " + str(count) )
+
+    if debug > 0:
         cv2.imshow("photo", image)
         cv2.waitKey(0)
+
     cv2.destroyAllWindows()
